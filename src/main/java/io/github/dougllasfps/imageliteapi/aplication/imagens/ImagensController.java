110717fd0f62bfa6a1c8d9261ef36b477461139c
marcospsantos226 <marcospsantos226@gmail.com>
2024-01-30 15:55:03 -0300

    Testando a pesquisa de imagens

--- src/main/java/io/github/dougllasfps/imageliteapi/aplication/imagens/ImagensController.java
+++ src/main/java/io/github/dougllasfps/imageliteapi/aplication/imagens/ImagensController.java
@@ -68,7 +68,7 @@ public class ImagensController {
     // localhost:8080/v1/images?extension=PNG&query=Nature
     @GetMapping
     public ResponseEntity<List<ImageDTO>> search
-    (@RequestParam(value = "extension", required = false) String extension,
+    (@RequestParam(value = "extension", required = false, defaultValue = "") String extension,
      @RequestParam(value = "query", required = false) String query){
 
         var result = service.search(ImageExtension.valueOf(extension), query);
